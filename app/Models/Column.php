e901d2eab4ebd3bb9cb09e98373772286b56ad05
spanol <viniciusspanol@gmail.com>
2025-08-19 15:20:50 -0300

    Relacionamento de entidades

---
+++ app/Models/Column.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Column extends Model
+{
+  use HasFactory;
+
+    protected $fillable = ['title', 'board_id', 'position'];
+
+    public function board()
+    {
+        return $this->belongsTo(Board::class);
+    }
+
+    public function cards()
+    {
+        return $this->hasMany(Card::class);
+    }
+}
